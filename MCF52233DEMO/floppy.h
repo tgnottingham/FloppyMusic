fe6a6dc4d968bdb55957fc7589845745b90095bf
Tyson Grant Nottingham <tgnottingham@gmail.com>
2013-02-24 00:11:00 -0800

    Renamed main.c, added missing floppy.h.

---
+++ MCF52233DEMO/floppy.h
@@ -0,0 +1,43 @@
+#ifndef _FLOPPY_H
+#define _FLOPPY_H
+
+#include "support_common.h"
+
+#define PIT0_PRESCALER 0
+// (50000 interrupts / sec. = 20 microsecond resolution)
+#define INTERRUPT_FREQUENCY 50000
+#define NUM_FLOPPIES 3
+#define NUM_TRACKS 80
+#define RESET_FLOPPIES_MESSAGE 128
+
+// FDD0 = PORTTC
+#define FDD0 ((vuint8*) (0x4010000F))
+#define FDD0_DIRECTION (0x4)
+#define FDD0_STEP (0x8)
+
+// FDD1 = PORTTC
+#define FDD1 ((vuint8*) (0x4010000F))
+#define FDD1_DIRECTION (0x1)
+#define FDD1_STEP (0x2)
+
+// FDD2 = PORTTA
+#define FDD2 ((vuint8*) (0x4010000E))
+#define FDD2_DIRECTION (0x1)
+#define FDD2_STEP (0x2)
+
+//#define FDD2 ((vuint8*) (0x40100011))
+//#define FDD2_DIRECTION 0x2
+//#define FDD2_STEP 0x4
+
+__declspec(interrupt:0) void timerHandler(void);
+void initializeFloppies();
+void initializeGPIO();
+void initializeInterrupts();
+void initializePIT();
+void initializeADC();
+uint16 getModulus(uint16 prescaler, uint32 frequency);
+void setFloppyFrequency(uint16 floppy, float frequency);
+void setFloppyPeriod(uint16 floppy, uint16 period);
+void resetFloppies();
+
+#endif
